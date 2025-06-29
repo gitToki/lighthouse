21d4811c3d6d8042d49e6ad358399231fa6b5ccb
gitToki <tokipro@proton.me>
2025-06-29 19:20:48 +0200

    benchmark

--- beacon_node/beacon_chain/benches/benches.rs
+++ beacon_node/beacon_chain/benches/benches.rs
@@ -1,17 +1,23 @@
 use std::sync::Arc;
 
-use beacon_chain::kzg_utils::{blobs_to_data_column_sidecars, reconstruct_data_columns};
-use beacon_chain::test_utils::get_kzg;
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-
-use bls::Signature;
-use kzg::{KzgCommitment, KzgProof};
+use beacon_chain::{
+    data_column_verification::validate_data_column_sidecar_for_gossip,
+    kzg_utils::blobs_to_data_column_sidecars,
+    observed_data_sidecars::DoNotObserve,
+    test_utils::{BeaconChainHarness, EphemeralHarnessType},
+};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use types::{
     beacon_block_body::KzgCommitments, BeaconBlock, BeaconBlockDeneb, Blob, BlobsList, ChainSpec,
-    EmptyBlock, EthSpec, KzgProofs, MainnetEthSpec, SignedBeaconBlock,
+    EmptyBlock, EthSpec, ForkName, KzgProofs, MainnetEthSpec, SignedBeaconBlock,
 };
+use bls::Signature;
+use kzg::{KzgCommitment, KzgProof};
 
-fn create_test_block_and_blobs<E: EthSpec>(
+type E = MainnetEthSpec;
+type T = EphemeralHarnessType<E>;
+
+fn create_test_block_and_blobs(
     num_of_blobs: usize,
     spec: &ChainSpec,
 ) -> (SignedBeaconBlock<E>, BlobsList<E>, KzgProofs<E>) {
@@ -32,36 +38,58 @@ fn create_test_block_and_blobs<E: EthSpec>(
     (signed_block, blobs, proofs)
 }
 
-fn all_benches(c: &mut Criterion) {
-    type E = MainnetEthSpec;
-    let spec = Arc::new(E::default_spec());
+fn setup_test_harness() -> BeaconChainHarness<T> {
+    let spec = ForkName::Fulu.make_genesis_spec(E::default_spec());
+    BeaconChainHarness::builder(E::default())
+        .spec(Arc::new(spec))
+        .deterministic_keypairs(64)
+        .fresh_ephemeral_store()
+        .mock_execution_layer()
+        .build()
+}
 
-    let kzg = get_kzg(&spec);
-    for blob_count in [1, 2, 3, 6] {
-        let (signed_block, blobs, proofs) = create_test_block_and_blobs::<E>(blob_count, &spec);
+fn bench_validate_data_column_sidecar_for_gossip(c: &mut Criterion) {
+    let harness = setup_test_harness();
+    let chain = &harness.chain;
+    let spec = &chain.spec;
+    let kzg = &chain.kzg;
 
+    let mut group = c.benchmark_group("validate_data_column_sidecar_for_gossip");
+    
+    for blob_count in [1, 5, 10, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 35, 40, 50] {
+        let (signed_block, blobs, proofs) = create_test_block_and_blobs(blob_count, spec);
+        
         let column_sidecars = blobs_to_data_column_sidecars(
             &blobs.iter().collect::<Vec<_>>(),
             proofs.to_vec(),
             &signed_block,
-            &kzg,
-            &spec,
+            kzg,
+            spec,
         )
         .unwrap();
 
-        let spec = spec.clone();
-
-        c.bench_function(&format!("reconstruct_{}", blob_count), |b| {
-            b.iter(|| {
-                black_box(reconstruct_data_columns(
-                    &kzg,
-                    &column_sidecars.iter().as_slice()[0..column_sidecars.len() / 2],
-                    spec.as_ref(),
-                ))
-            })
-        });
+        if let Some(data_column) = column_sidecars.first() {
+            let data_column = Arc::new(data_column.clone());
+            let subnet = 0u64;
+            
+            group.bench_with_input(
+                BenchmarkId::new("blobs", blob_count),
+                &blob_count,
+                |b, _| {
+                    b.iter(|| {
+                        let _ = black_box(validate_data_column_sidecar_for_gossip::<_, DoNotObserve>(
+                            (*data_column).clone(),
+                            subnet,
+                            chain,
+                        ));
+                    })
+                },
+            );
+        }
     }
+    
+    group.finish();
 }
 
-criterion_group!(benches, all_benches);
+criterion_group!(benches, bench_validate_data_column_sidecar_for_gossip);
 criterion_main!(benches);
